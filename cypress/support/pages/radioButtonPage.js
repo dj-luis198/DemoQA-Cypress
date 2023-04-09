1c137f0707ef4f96c8b5ded408e9bb04cd8dc3a5
dj-luis198 <dj.luis198@gmail.com>
2023-04-09 15:21:22 -0300

    radio button

---
+++ cypress/support/pages/radioButtonPage.js
@@ -0,0 +1,30 @@
+export class RadioButtonPage{
+
+    constructor(){
+        this.radioButton='#item-2';
+        this.yesRadioButton='#yesRadio[type="radio"]';
+        this.impressiveButton='#impressiveRadio';
+        this.noButton="#noRadio";
+        this.successText='.text-success';
+    }
+
+    clickRadioButton(){
+        cy.gClick(this.radioButton);
+    }
+
+    selectYes(){
+        return cy.gCheck(this.yesRadioButton);
+    }
+
+    returnSuccessText(){
+        return cy.get(this.successText);
+    }
+
+    clickImpressive(){
+        return cy.gCheck(this.impressiveButton);
+    }
+
+    returnNoOption(){
+        return cy.get(this.noButton);
+    }
+}

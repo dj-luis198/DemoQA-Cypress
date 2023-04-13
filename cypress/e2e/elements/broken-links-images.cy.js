1a04eefd7bfaaa5423ee6135fba37dce123ecadf
dj-luis198 <dj.luis198@gmail.com>
2023-04-13 01:13:41 -0300

    broken-links-images

---
+++ cypress/e2e/elements/broken-links-images.cy.js
@@ -0,0 +1,28 @@
+///<reference types="cypress" />
+
+import { HomePage } from "../../support/pages/homePage";
+import { BrokenLinksImagesPage } from "../../support/pages/brokenLinksImagesPage";
+
+describe('Broken Links - Images', () => {
+    const homePage = new HomePage();
+    const brokenLinksImagesPage = new BrokenLinksImagesPage();
+
+    beforeEach(() => {
+        cy.visit('/');
+        homePage.clickElements();
+        brokenLinksImagesPage.clickBrokenLinks();
+    })
+
+    it('Verificar imagenes de la pagina', () => {
+        brokenLinksImagesPage.returnImageImg().each(img=>{
+            const href=img.prop('naturalHeight');
+            expect(href).is.greaterThan(0);
+        })
+    })
+
+    it('Verificar links de la pagina', () => {
+        brokenLinksImagesPage.returnLinks().each(page=>{
+            cy.request(page.prop('href'))
+        })
+    })
+})
---
+++ cypress/support/pages/brokenLinksImagesPage.js
@@ -0,0 +1,20 @@
+export class BrokenLinksImagesPage{
+
+    constructor(){
+        this.brokenLinks='#item-6';
+        this.imageImg='img[src*="images"]';
+        this.links='a[href]';
+    }
+
+    clickBrokenLinks(){
+        cy.gClick(this.brokenLinks);
+    }
+
+    returnImageImg(){
+        return cy.get( this.imageImg);
+    }
+
+    returnLinks(){
+        return cy.get(this.links);
+    }
+}

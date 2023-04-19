aff9aee41146a908716039edcab8189c73e2e571
dj-luis198 <dj.luis198@gmail.com>
2023-04-19 12:06:41 -0300

    nested frames

---
+++ cypress/support/pages/nestedFramesPage.js
@@ -0,0 +1,16 @@
+export class NestedFamesPage {
+
+    constructor() {
+        this.nestedFramesButton = 'Nested Frames';
+        this.frame1 = '#frame1';
+        this.frame2 = 'iframe';
+    };
+
+    clickNestedFrames() {
+        cy.contains('span[class="text"]', this.nestedFramesButton).click();
+    };
+
+    returnFrameChildren() {
+        return cy.getIframeChildrenBody(this.frame1, this.frame2).find('p');
+    };
+};

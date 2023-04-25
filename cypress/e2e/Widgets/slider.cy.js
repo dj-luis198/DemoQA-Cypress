54944e4aba0a7f7013171d454d284964b5d24d72
dj-luis198 <dj.luis198@gmail.com>
2023-04-24 22:41:21 -0300

    Slider tipos y formas deres.

---
+++ cypress/e2e/Widgets/slider.cy.js
@@ -0,0 +1,29 @@
+//https://www.youtube.com/watch?v=zL9PxwQrRtQ
+//https://qaboxletstestcypresspracticesite.netlify.app/
+//How to work with Sliders in Cypress?
+//1. Slider of Input type equal to Range
+//2. Customized Slider Components
+//3. Cypress Commands used
+//           a. .invoke('val')
+//          b. .invoke('attr') &
+//          c. .trigger('event-name')
+
+///<reference types="cypress" />
+import { HomePage } from "../../support/pages/homePage";
+import { SliderPage } from "../../support/pages/sliderPage";
+
+describe('Slider', () => {
+    const homePage = new HomePage();
+    const sliderPage = new SliderPage();
+
+    beforeEach(() => {
+        cy.visit('/');
+        homePage.clickWidgetsCard();
+        sliderPage.clickSlider();
+    });
+
+    it('slider input type= range utilizando stepUp', () => {
+        sliderPage.moveSlider(90);
+        sliderPage.returnValueSlider().should('have.value', 90);
+    });
+});

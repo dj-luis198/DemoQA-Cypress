92f54301004525dcb2f56814584e22eb0ad4d270
dj-luis198 <dj.luis198@gmail.com>
2023-05-10 18:24:53 -0300

    add axe, Accessibility violations to book store

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -147,3 +147,36 @@ Cypress.Commands.add('loginGUI', (loginData) => {
     cy.url().should('contain', '/profile')
   })
 })
+
+// Custom AXE-CORE Logging
+
+Cypress.Commands.add('customCheckAlly', () => {
+  const severityIndicatorIcons = {
+    minor: '⚪',
+    moderate: '🌕',
+    serious: '⭕',
+    critical: '⛔',
+  }
+  function callback(violations) {
+    violations.forEach((violation) => {
+      const nodes = Cypress.$(
+        violation.nodes.map((node) => node.target).join(',')
+      )
+      Cypress.log({
+        name: `${severityIndicatorIcons[violation.impact]} AllY`,
+        consoleProps: () => violation,
+        $el: nodes,
+        message: `[${violation.help}](${violation.helpUrl})`,
+      })
+      violation.nodes.forEach(({ target }) => {
+        Cypress.log({
+          name: 'ℹ▶',
+          consoleProps: () => violation,
+          $el: Cypress.$(target.join(',')),
+          message: target,
+        })
+      })
+    })
+  }
+  cy.checkA11y(null, null, callback)
+})
